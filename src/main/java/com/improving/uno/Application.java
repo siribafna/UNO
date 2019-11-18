ebe9fc7b2500e40ad1473e33c48a1006ef8d06e0
Siri Bafna <Siri.Bafna@improving.com>
2019-11-18 09:21:04 -0600

    testing second commit

--- src/main/java/com/improving/uno/Application.java
+++ src/main/java/com/improving/uno/Application.java
@@ -6,6 +6,7 @@ public class Application {
     public static void main(String[] args)  {
         System.out.println("Welcome to UNO!");
         System.out.print("How many players? ");
+        System.out.print("Testing my second commit");
         Scanner scan = new Scanner(System.in);
 
         int playersNum = scan.nextInt();
